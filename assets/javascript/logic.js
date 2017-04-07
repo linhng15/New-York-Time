2f356db6c4173ed06f6703dab7b9915e188e695c
DKMitt <dkmitt@sbcglobal.net>
2017-04-06 23:50:05 -0500

    cleaned up code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -58,7 +58,7 @@ function runQuery(numArticles, queryURL) {
                 $("#articleWell-" + i).append("<h5>" + NYTData.response.docs[i].section_name + "</h5>");
                 $("#articleWell-" + i).append("<h5>" + NYTData.response.docs[i].pub_date + "</h5>");
                 $("#articleWell-" + i).append("<a href=" + NYTData.response.docs[i].web_url + ">" + NYTData.response.docs[i].web_url + "</a>");
-            	
+
             }
 
             console.log(queryURL);
